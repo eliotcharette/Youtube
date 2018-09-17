52217ef411be5964d314bf05cc3ae8f43bc3dcd5
Eliot Charette <eliotcharette@gmail.com>
2018-09-16 21:25:07 -0700

    Add content to video_list to give unique identifiers to each video searched

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCFO39RM4Zj02FGUpvdoxaf9uIfoIKZBJU';
 
-//Create a ndew component. this component should procdue
-//some html
-const App = () => {
-  return (
-  <div>
-    <SearchBar />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { videos: [] };
+    
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });  
+  }
+  
+  render(){
+    return (
+      <div>
+      <SearchBar />
+      <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-//Take this components generate HTML and
-//Tput it on the page (in the DOM)
 ReactDom.render(<App />, document.querySelector('.container'))
